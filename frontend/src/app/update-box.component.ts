5ad7d10f603db381bc1479d526182538b7541cf8
hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
2023-10-11 17:17:04 +0200

    Update box siger den er undefined fejl er i box service tror jeg

--- frontend/src/app/update-box.component.ts
+++ frontend/src/app/update-box.component.ts
@@ -2,7 +2,7 @@
 import {Component, NgModule} from "@angular/core";
 import {BoxService} from"./box.service"
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
-import {Box, ResponseDto} from "../models";
+import {Box} from "../models";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {environment} from "../environments/environment";
 import {State} from "../state";
@@ -14,7 +14,7 @@ import {ModalController, ToastController} from "@ionic/angular";
 
   <ion-list>
     <ion-item>
-      <ion-input [formControl]="updateBoxForm.controls.boxTitle" data-testid="titleInput">
+      <ion-input [formControl]="updateBoxForm.controls.boxTitle" data-testid="titleInput" label="Insert new title for box">
         <div *ngIf="!updateBoxForm.controls.boxTitle.valid">box title must be 4 chars or more</div>
       </ion-input>
     </ion-item>
@@ -57,11 +57,12 @@ import {ModalController, ToastController} from "@ionic/angular";
 })
 export class UpdateBoxComponent {
   boxElement: Box | undefined;
-  constructor(public boxService: BoxService, public fb: FormBuilder, public modalController: ModalController, public http: HttpClient, public state: State, public toastController: ToastController) {
+  constructor(public fb: FormBuilder, public modalController: ModalController, public http: HttpClient, public state: State, public toastController: ToastController,public boxService: BoxService) {
   }
 
   ngOnInit(){
     this.boxService.currentNumber.subscribe(boxElement=>this.boxElement=boxElement)
+    console.log(this.boxService.currentNumber.subscribe(boxElement=>this.boxElement=boxElement))
   }
 
   boxTitle = new FormControl(this.boxService.box?.boxTitle, [Validators.minLength(4), Validators.required])
@@ -92,9 +93,10 @@ export class UpdateBoxComponent {
 
   async submitUpdate() {
     let boxNumber1 = this.boxElement?.boxId
+    console.log(boxNumber1)
 
     try {
-      const observable =     this.http.put<Box>(environment.baseUrl + '/api/boxes', this.updateBoxForm.getRawValue())
+      const observable =     this.http.put<Box>(environment.baseUrl + '/api/boxes/' + boxNumber1, this.updateBoxForm.getRawValue())
       const response = await firstValueFrom(observable);
       const boxId = this.state.boxes.findIndex(box=> box.boxId == response.boxId)
       this.state.boxes[boxId] = response;
